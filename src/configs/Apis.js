0ee0e707ae425703eac6826c7fdd35cf063f6187
TaiMesut <nguyenthanhtaiabcxyz@gmail.com>
2025-05-09 10:14:03 +0700

    update

--- src/configs/Apis.js
+++ src/configs/Apis.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 import cookie from 'react-cookies'
 
-const BASE_URL = 'http://54.81.10.206:8080/DienToanDamMay';
+const BASE_URL = 'http://back-end-457180940.us-east-1.elb.amazonaws.com/DienToanDamMay';
 
 export const endpoints = {
     'register': '/register',
